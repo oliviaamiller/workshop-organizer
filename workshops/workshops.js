6722b7fdde2f6d69b40ce834e6341709eedfde43
oliviaamiller <oliviaoliviaolivia@gmail.com>
2022-01-11 15:51:34 -0800

    added create participant button that links to create page

--- workshops/workshops.js
+++ workshops/workshops.js
@@ -3,7 +3,12 @@ import { checkAuth, logout } from '../fetch-utils.js';
 checkAuth();
 
 const logoutButton = document.getElementById('logout');
+const participantButton = document.getElementById('add-participant');
 
 logoutButton.addEventListener('click', () => {
     logout();
+});
+
+participantButton.addEventListener('click', () => {
+    window.location.href = '../create';
 });
