6adaeb2f968867226cd73e7e12f22a469cd096ad
oliviaamiller <oliviaoliviaolivia@gmail.com>
2022-01-11 17:40:01 -0800

    create page workshops dynamic dropdown working

--- create/create.js
+++ create/create.js
@@ -1,8 +1,50 @@
-import { checkAuth, logout } from '../fetch-utils.js';
+import { 
+    checkAuth, 
+    logout, 
+    createParticipant,
+    getWorkshops
+} from '../fetch-utils.js';
 
 checkAuth();
 
+const form = document.querySelector('form');
 const logoutButton = document.getElementById('logout');
+
+form.addEventListener('submit', async(e) => {
+    e.preventDefault();
+
+    const data = new FormData(form);
+
+    const participantName = data.get('name');
+    const workshopId = data.get('workshop-id');
+
+    await createParticipant({
+        name: participantName,
+        workshop_id: workshopId
+    });
+    
+    form.reset();
+});
+
+window.addEventListener('load', async() => {
+
+    const workshopDropdownEl = document.querySelector('select');
+
+    const workshops = await getWorkshops();
+
+    for (let workshop of workshops) {
+        const optionEl = document.createElement('option');
+
+        optionEl.value = workshop.id;
+
+        optionEl.textContent = workshop.name;
+
+        workshopDropdownEl.append(optionEl);
+    }
+});
+
+
+checkAuth();
 
 logoutButton.addEventListener('click', () => {
     logout();
